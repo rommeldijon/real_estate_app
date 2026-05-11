eb1632b72a97cbad93fdc2fea2ddcee6a8f2941d
Rommel Dijon <rdijon@ymail.com>
2026-05-11 12:29:20 -0700

    completed asset and font set up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,26 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 import "./globals.css";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+  })
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return <Stack />;
 }
